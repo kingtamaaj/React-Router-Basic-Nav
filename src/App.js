835a9b0c4c10316acbb3be92e02f972ef1cd31c1
kingtamaaj <kingtamaaj@gmail.com>
2019-05-20 18:39:13 -0400

    Updated Navigation with with links and updated index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
+    <Home />
+    <About />
     <Navigation />
+    <Contact />
   </div>
 );
+
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
-        <h1>React Router Mini</h1>
+        <h1 className="app=header">React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link exact className="home-link" to="/">Home </Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link exact className="about-link" to="/about"> About </Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link exact className="contact" to="/contact"> Contact</Link>
         </div>
       </div>
     </div>
